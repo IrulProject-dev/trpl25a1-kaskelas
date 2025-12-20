b9b9301555090fe47edff44c522eae1411cb58e3
IrulProject-Dev <irulproject77@gmail.com>
2025-12-20 13:46:49 +0700

    url-update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Member;
 use App\Observers\MemberObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
